48e847c265560600d2b78decaf41cb02124cd002
evasancho19 <eva.sancho@alumnos.upm.es>
2023-04-27 19:16:21 +0200

    Update prueba1.c

--- prueba1.c
+++ prueba1.c
@@ -1,3 +1,22 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
+{
+	int paso1;
+	int paso;
+	printf("TITULO \n");
+	printf("Indique 1 si quiere hacer una consulta individual o 2 si quiere hacer una comparativa \n");
+	scanf("%i", &paso);
+	switch(paso)
+	{
+		case 1:
+			printf("Indique la energía a consultar: \n 1-Hidráulica \n 2-Turbinación bombeo \n 3- ");
+			break;
+		case 2:
+		    break; 	
+		default: 
+		    printf("Número no válido, vuelva a ejecutar el progama");// no se poner para que vuelva al inicio    
+	}
+}
+
