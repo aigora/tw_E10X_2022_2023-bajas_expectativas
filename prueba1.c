c41077d11ac09422ba39887b4e1a58d9fcbb721b
evasancho19 <eva.sancho@alumnos.upm.es>
2023-05-13 20:54:40 +0200

    Update prueba1.c

--- prueba1.c
+++ prueba1.c
@@ -20,8 +20,8 @@ energia tipoenergia[18];
 float i;
 int j, k;
 FILE *plectura;
-plectura = fopen("C:/Users/albag/OneDrive/Documentos/GitHub/tw_E10X_2022_2023-bajas_expectativas/generacion_por_tecnologias_21_22.txt", "r");
-
+//plectura = fopen("C:/Users/albag/OneDrive/Documentos/GitHub/tw_E10X_2022_2023-bajas_expectativas/generacion_por_tecnologias_21_22.txt", "r");
+plectura = fopen("C:/Users/34689/OneDrive/Documentos/GitHub/tw_E10X_2022_2023-bajas_expectativas/generacion_por_tecnologias_21_22.txt", "r");
 if (plectura == NULL)
 {
 printf("ERROR AL ABRIR EL FICHERO DE LECTURA");
